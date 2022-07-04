377ad67a50eda2c447778d8e90d0fa048f784c75
envydiace <ninhducanh216@gmail.com>
2022-07-04 21:56:16 +0700

    new code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //comment code
+        //new code
         setContentView(R.layout.activity_main);
     }
 }
