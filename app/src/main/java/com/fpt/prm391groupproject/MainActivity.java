5bd1dffb1ab54cf3d53706f31e66256b6555d0f3
envydiace <ninhducanh216@gmail.com>
2022-07-08 17:03:59 +0700

    add update delete model

--- app/src/main/java/com/fpt/prm391groupproject/MainActivity.java
+++ app/src/main/java/com/fpt/prm391groupproject/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.fpt.prm391groupproject.DAO.ProductDAO;
 import com.fpt.prm391groupproject.model.Product;
@@ -18,25 +21,31 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    ProductDAO dao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dao = new ProductDAO();
         setContentView(R.layout.activity_main);
     }
 
     public void onClickAddBtn(android.view.View v){
         Product p = new Product("Laptop 3",250,30);
-        p.setId("MrcCtkVQDReSj1KQk5mh");
-        ProductDAO dao = new ProductDAO();
-//        dao.updateProduct(p);
-//        dao.addProduct(p);
-//        List<Product> list = dao.getProducts();
-//        for (Product p: list){
-//            System.out.println(p);
-//        }
-//        Product product = dao.getProductById("KXsOYOBf7QmSeq4D4OEa");
-//        System.out.println(product!=null ? product.getProductName():"null");
-        dao.deleteProduct("MrcCtkVQDReSj1KQk5mh");
+        dao.addProduct(p);
+    }
+
+    public void onClickUpdateBtn(android.view.View v){
+
+    }
+
+    public void onClickDeleteBtn(android.view.View v){
+        EditText text = findViewById(R.id.tbId);
+        String id = text.getText().toString();
+        dao.deleteProduct(id);
+    }
+
+    public void onClickViewBtn(android.view.View v){
+
     }
 
 
