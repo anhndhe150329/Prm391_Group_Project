196ceb50ff60384b2ec9b59c1dfe6f9b0e63d10b
envydiace <ninhducanh216@gmail.com>
2022-07-19 22:04:22 +0700

    test change fragment

---
+++ app/src/main/java/com/fpt/prm391groupproject/HomeActivity.java
@@ -0,0 +1,109 @@
+package com.fpt.prm391groupproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.fpt.prm391groupproject.DAO.ProductDAO;
+import com.fpt.prm391groupproject.fragment.HomeFragment;
+import com.fpt.prm391groupproject.fragment.ProfileFragment;
+import com.fpt.prm391groupproject.model.Product;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int FRAGMENT_HOME = 0;
+    private static final int FRAGMENT_PROFILE = 1;
+
+    private int currentFragment = FRAGMENT_HOME;
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navi_drawer_open,R.string.navi_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        replaceFragment(new HomeFragment());
+        navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.nav_home:
+                if (currentFragment != FRAGMENT_HOME){
+                    replaceFragment(new HomeFragment());
+                    currentFragment = FRAGMENT_HOME;
+                }
+                break;
+            case R.id.nav_profile:
+                if (currentFragment != FRAGMENT_PROFILE){
+                    replaceFragment(new ProfileFragment("come from home fragment"));
+                    currentFragment = FRAGMENT_PROFILE;
+                }
+
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame,fragment);
+        transaction.commit();
+    }
+
+
+
+}
--- app/src/main/java/com/fpt/prm391groupproject/ProductAdapter.java
+++ app/src/main/java/com/fpt/prm391groupproject/ProductAdapter.java
@@ -1,16 +1,22 @@
 package com.fpt.prm391groupproject;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.fpt.prm391groupproject.fragment.ProfileFragment;
 import com.fpt.prm391groupproject.model.Product;
 
 import java.util.List;
@@ -18,22 +24,25 @@ import java.util.List;
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder>{
     private List<Product> products;
     private Context context;
+    private FragmentActivity fragmentActivity;
 
-    public ProductAdapter(List<Product> products, Context context) {
+    public ProductAdapter(List<Product> products, FragmentActivity fragmentActivity) {
         this.products = products;
-        this.context = context;
+        this.fragmentActivity = fragmentActivity;
     }
 
     @NonNull
     @Override
     public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
         View v = LayoutInflater.from(context).inflate(R.layout.item_product, parent, false);
         return new ProductViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
-        holder.imv.setImageResource(products.get(position).getId_image());
+        int imgId = context.getResources().getIdentifier(products.get(position).getImage(), "drawable", context.getPackageName());
+        holder.imv.setImageResource(imgId);
         holder.tv_name.setText(products.get(position).getProductName());
         holder.tv_price.setText("$ "+products.get(position).getPrice());
     }
@@ -55,6 +64,27 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             tv_name = itemView.findViewById(R.id.tv_product_name);
             tv_price = itemView.findViewById(R.id.tv_product_price);
             btn_add =  itemView.findViewById(R.id.btn_add_to_cart);
+            btn_add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    String id =products.get(getAdapterPosition()).getId();
+
+                    Toast.makeText(view.getContext(),id,Toast.LENGTH_SHORT).show();
+
+                    FragmentTransaction transaction = fragmentActivity.getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.content_frame,new ProfileFragment(id));
+                    transaction.commit();
+
+                }
+            });
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(view.getContext(),"Clicked on: "+(getAdapterPosition()+1),Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
