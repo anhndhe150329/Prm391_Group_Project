08abc33e039ed5ba0d807cb8986cf99c2e15d176
envydiace <ninhducanh216@gmail.com>
2022-07-20 18:39:36 +0700

    register user

--- app/src/main/java/com/fpt/prm391groupproject/Utils/Constants.java
+++ app/src/main/java/com/fpt/prm391groupproject/Utils/Constants.java
@@ -13,6 +13,16 @@ public class Constants {
         public static final String productFavourite = "Favourite";
     }
 
+    public static class FireBaseUserTable{
+        public static final String dbName = "User";
+        public static final String userName = "Name";
+        public static final String userEmail = "Email";
+        public static final String userId = "UserId";
+        public static final String userPhone = "Phone";
+        public static final String userAddress = "Address";
+        public static final String userAge = "Age";
+    }
+
     public static class SQLiteCategoryTable{
         public static final String tableName = "Category";
         public static final String id = "id";
