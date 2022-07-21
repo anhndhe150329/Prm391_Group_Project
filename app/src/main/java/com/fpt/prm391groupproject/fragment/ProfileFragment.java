1f94d1d1ef068d1ebfd066bb61f3e4ff6bacd34e
envydiace <ninhducanh216@gmail.com>
2022-07-21 17:02:17 +0700

    profile

--- app/src/main/java/com/fpt/prm391groupproject/fragment/ProfileFragment.java
+++ app/src/main/java/com/fpt/prm391groupproject/fragment/ProfileFragment.java
@@ -15,7 +15,7 @@ import com.fpt.prm391groupproject.R;
 import com.fpt.prm391groupproject.model.User;
 
 public class ProfileFragment extends Fragment {
-    TextView textView;
+    TextView name,email,phone,age,address;
     private String id;
 
     private UserSQLiteDAO userSQLiteDAO;
@@ -30,6 +30,16 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile,container,false);
         userSQLiteDAO = new UserSQLiteDAO(container.getContext());
         User user = userSQLiteDAO.getUserById(id);
+        name = view.findViewById(R.id.txtName);
+        email = view.findViewById(R.id.txtEmail);
+        phone = view.findViewById(R.id.txtPhone);
+        age = view.findViewById(R.id.txtAge);
+        address = view.findViewById(R.id.txtAddress);
+        name.setText(user.getName());
+        email.setText(user.getEmail());
+        phone.setText(user.getPhone());
+        age.setText(user.getAge()+"");
+        address.setText(user.getAddress());
         return view;
     }
 }
